267140c5d7314605b53c50a99dec6a75e41b7264
Batuhan Özdemir <121499687+androcyber@users.noreply.github.com>
2023-03-28 16:20:53 +0300

    Dockerfile

---
+++ Minecraft/PocketMine-MP/run.sh
@@ -0,0 +1,18 @@
+#
+#       Social Media
+#   youtube.com/androcyber
+#   twitch.com/androcyber
+#   twitter.com/androcyber0
+#   instagram.com/androcyber0
+#
+#   Made by androcyber
+#
+cd /home/container
+
+# Startup message
+echo "-------------------------------------------------------------------------------------------------------------"
+echo "Hostibu | Sunucu başlatılıyor..."
+echo "-------------------------------------------------------------------------------------------------------------"
+
+# Run the server
+eval ${STARTUP}
