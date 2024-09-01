db050b7d986344e0e0c2c9d14473b3223acb7bda
Batuhan Özdemir <121499687+batuhnzdmr@users.noreply.github.com>
2024-09-01 18:42:07 +0300

    Dockerfile

--- Minecraft/PocketMine-MP/run.sh
+++ Minecraft/PocketMine-MP/run.sh
@@ -6,20 +6,19 @@ echo "--------------------------------------------------------------------------
 echo "Hostibu | Sunucu güncellemeleri denetleniyor..."
 echo "-------------------------------------------------------------------------------------------------------------"
 
-#rm Pocketmine-MP.phar
-#rm pocketmine.yml
+rm Pocketmine-MP.phar
 
-# Check server version
-#if [[ "${SERVER_VERSION}" == "latest" ]]; then
-#    wget -q "https://update.pmmp.io/api" -O pocketmine
-#    link=$(grep -i 'download_url' pocketmine | cut -d '"' -f 4)
-#    rm api
-#else
-#    link="https://github.com/pmmp/PocketMine-MP/releases/download/${SERVER_VERSION}/PocketMine-MP.phar"
-#fi
+# Check version
+if [[ "${SERVER_VERSION}" == "latest" ]]; then
+    wget -q "https://update.pmmp.io/api" -O api.json
+    link=$(jq -r '.download_url' api.json)
+    rm api.json
+else
+    link="https://github.com/pmmp/PocketMine-MP/releases/download/${SERVER_VERSION}/PocketMine-MP.phar"
+fi
 
-# Download server file
-#wget -q $link -O "PocketMine-MP.phar"
+# Download server files
+wget -q $link -O "PocketMine-MP.phar"
 
 # File permissions
 chmod +x bin/php7/bin/php
