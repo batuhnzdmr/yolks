83d067803e88e8ebf46b795c515324a6947f2963
Batuhan Özdemir <121499687+batuhnzdmr@users.noreply.github.com>
2024-09-01 18:16:24 +0300

    Dockerfile

--- Minecraft/PocketMine-MP/run.sh
+++ Minecraft/PocketMine-MP/run.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-cd /hostibu/server
+cd /home/hostibu
 
 # Check for updates
 echo "-------------------------------------------------------------------------------------------------------------"
 echo "Hostibu | Sunucu güncellemeleri denetleniyor..."
 echo "-------------------------------------------------------------------------------------------------------------"
 
-rm Pocketmine-MP.phar
-rm pocketmine.yml
+#rm Pocketmine-MP.phar
+#rm pocketmine.yml
 
 # Check server version
-if [[ "${SERVER_VERSION}" == "latest" ]]; then
-    wget -q "https://update.pmmp.io/api" -O pocketmine
-    link=$(grep -i 'download_url' pocketmine | cut -d '"' -f 4)
-    rm api
-else
-    link="https://github.com/pmmp/PocketMine-MP/releases/download/${SERVER_VERSION}/PocketMine-MP.phar"
-fi
+#if [[ "${SERVER_VERSION}" == "latest" ]]; then
+#    wget -q "https://update.pmmp.io/api" -O pocketmine
+#    link=$(grep -i 'download_url' pocketmine | cut -d '"' -f 4)
+#    rm api
+#else
+#    link="https://github.com/pmmp/PocketMine-MP/releases/download/${SERVER_VERSION}/PocketMine-MP.phar"
+#fi
 
 # Download server file
-wget -q $link -O "PocketMine-MP.phar"
+#wget -q $link -O "PocketMine-MP.phar"
 
 # File permissions
 chmod +x PocketMine-MP.phar
